0e6ed33b7e030c4b79770a68fc0b73052388b20f
janeffer945 <janeffernjeri945@gmail.com>
2022-05-17 19:54:20 +0300

    import flask

--- app/request.py
+++ app/request.py
@@ -1,7 +1,10 @@
-import requests,json
+# import requests,json
+
+from flask import request_finished
+
 
 def get_quotes():
-    response = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
+    response = request_finished.get('http://quotes.stormconsultancy.co.uk/random.json')
     if response.status_code == 200:
         quote = response.json()
         # print(quote)
---
+++ config.py
@@ -0,0 +1,44 @@
+import os
+
+class Config:
+    # UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    # API_KEY = os.environ.get('API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+   # simple mde configuration
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+     #mail cconfiguration
+
+
+    MAIL_SERVER= 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS=True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD= os.environ.get("MAIL_PASSWORD")
+
+    
+
+
+# class TestConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://'
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    
+
+
+class DevConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Jenny@localhost/blogdb'
+    DEBUG = True
+
+    config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+# 'test':TestConfig
+}
