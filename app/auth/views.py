cac2c99a5b6725568966ac0ccd31dbd717d3e245
janeffer945 <janeffernjeri945@gmail.com>
2022-05-18 14:29:53 +0300

    added registration form

--- app/auth/views.py
+++ app/auth/views.py
@@ -21,3 +21,26 @@ def login():
 
     title = "watchlist login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to KalebsBlog","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)    
+
+
+#....
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
