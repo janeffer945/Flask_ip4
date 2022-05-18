a7f9cb241d55b2a0651ccbdcc0a18ebc9f8ad153
janeffer945 <janeffernjeri945@gmail.com>
2022-05-18 14:35:43 +0300

    added update profile

---
+++ app/main/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import DataRequired
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
+    submit = SubmitField('Submit')
+class BlogForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    category = SelectField('Category', choices=[('Technology','Technology'),('Health','Health'),('Food','Food '),('Lifestyle','lifestyle'),],validators=[DataRequired()])
+    post = TextAreaField('Your Story', validators=[DataRequired()])
+    submit = SubmitField('Post Blog')  
