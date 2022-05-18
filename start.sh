17142d3819858c359b5aab5092148775d24aa366
janeffer945 <janeffernjeri945@gmail.com>
2022-05-18 16:12:49 +0300

    final

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export DATABASE_URL='postgresql+psycopg2://moringa:Jenny@localhost/blogdb'
 export MAIL_USERNAME=janeffernjeri945@gmail.com
 export MAIL_PASSWORD=janeffernjerikinyua
-python3.8 manager.py server
+python3.8 manage.py server
