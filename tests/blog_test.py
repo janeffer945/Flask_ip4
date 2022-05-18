4ff75ff64363685de098615e1f61c0d0e1c0addc
janeffer945 <janeffernjeri945@gmail.com>
2022-05-18 15:01:00 +0300

    add comment

--- tests/blog_test.py
+++ tests/blog_test.py
@@ -6,7 +6,7 @@ class BlogModelTest(unittest.TestCase):
     def setUp(self):
         self.user_charles = User(username='janeffer',password='123456789',email='test@test.com')
         self.new_blog = Blog(id=1, title='Test', content='This is a test blog', user_id=self.user_charles.id)
-     def tearDown(self):
+    def tearDown(self):
         Blog.query.delete()
         User.query.delete()
 
